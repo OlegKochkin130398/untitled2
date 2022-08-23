c76cb668a8101ad0807de6a9e6c78228df9d77cf
oleg- <o.boys@yandex.ru>
2022-08-23 13:29:24 +0300

    Задание 2.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 5;i<=31;i=i+7)
+            System.out.println("Сегодня пятница,"+i+"-е число. Необходимо подготовить отчет.");
+    }
+}
